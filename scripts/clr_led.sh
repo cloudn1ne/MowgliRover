c46afb2367a613c390f780006be785841228bf32
Ubuntu <cn@warp.at>
2022-09-25 23:34:18 +0200

    nicer shell scripts to set/clr leds

--- scripts/clr_led.sh
+++ scripts/clr_led.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-echo $1
-rosservice call /mowgli/ClrLed "{ led: $1 }"
+if [ $# -lt 1 ];
+then 
+   echo ""
+   echo "$0 <led_number> <chirp> <clearall>"
+   echo ""
+   echo " <led_number> 1-18 for YF500C"
+   echo " <chirp> 1 or 0"
+   echo " <clearall> 1 or 0"
+   echo ""
+   exit -1
+fi
+
+v=$1
+# chirp flag
+if [ "$2" == "1" ];
+then
+  v=$((v + 128))
+fi
+# chirp flag
+if [ "$3" == "1" ];
+then
+  v=$((v + 64))
+fi
+
+echo "sending $v"
+rosservice call /mowgli/ClrLed "{ led: $v }"
